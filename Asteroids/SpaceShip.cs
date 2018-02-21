5bd9d91a51a9ec32c07ecf9bd7cb28d5c45a96ca
Martin Nilsson <martin.e.nilsson@hotmail.com>
2018-02-21 20:37:05 +0100

    Signed-off-by: Martin Nilsson <martin.e.nilsson@hotmail.com>

---
+++ Asteroids/SpaceShip.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Asteroids
+{
+    public class SpaceShip : Sprite
+    {
+        private readonly Texture2D texture;
+        private readonly Vector2 location;
+        private readonly Color color;
+        private float speed = 0.1f;
+        private float turningSpeed = MathHelper.Pi * 2 / 100;
+        private float circle = MathHelper.Pi * 2;
+
+
+        public SpaceShip(Texture2D texture, Vector2 location, Color color) : base(texture, location, color)
+        {
+            this.texture = texture;
+            this.location = location;
+            this.color = color;
+        }
+
+        public void Stabilize()
+        {
+            Vector2 direction = Velocity;
+            direction.Normalize();
+            Velocity.X = Velocity.X + (Velocity.X / -Velocity.X) * direction.X * speed*2;
+            Velocity.Y = Velocity.Y + (Velocity.Y / -Velocity.Y) * direction.Y * speed*2;
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            Vector2 direction = new Vector2((float)Math.Cos(RotationAngle), (float)Math.Sin(RotationAngle));
+            direction.Normalize();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                Velocity += direction * speed;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                Velocity -= direction * speed;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                RotationAngle -= turningSpeed % circle;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                RotationAngle += turningSpeed % circle;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+                Stabilize();
+
+            base.Update(gameTime);
+        }
+    }
+}
---
+++ Asteroids/Sprite.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroids
+{
+    public class Sprite
+    {
+        private readonly Texture2D texture;
+        public Vector2 Location;
+        private readonly Color color;
+        public Vector2 Velocity = Vector2.Zero;
+        public float RotationAngle = 0;
+        public Rectangle sourceRectangle;
+        public Vector2 origin;
+
+        public Sprite(Texture2D texture, Vector2 location, Color color)
+        {
+            this.texture = texture;
+            Location = location;
+            this.color = color;
+            
+    }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Rectangle sourceRectangle = new Rectangle(0, 0, texture.Width, texture.Height);
+            Vector2 origin = new Vector2(texture.Width / 2, texture.Height / 2);
+            spriteBatch.Draw(texture,Location,sourceRectangle, color, RotationAngle, origin, 0.5f, SpriteEffects.None,1);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            Location += Velocity;
+        }
+
+
+
+
+
+    }
+}
